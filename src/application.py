e42727bd72d50657cbd87460490d4948d6c46d17
Artur Cepuc <arturcepuc@gmail.com>
2021-06-29 18:02:11 +0300

    New app version & AWS Region

--- src/application.py
+++ src/application.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! v1.1"
 
 # run the app.
 if __name__ == "__main__":
--- tests/test_application.py
+++ tests/test_application.py
@@ -4,5 +4,5 @@ from application import application
 
 with application.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello World!'
+    assert response.data == b'Hello World! v1.1'
     assert response.status_code == 200
