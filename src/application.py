24bdbab663cff6ceeb2f0330137226990f461186
Artur Cepuc <arturcepuc@gmail.com>
2021-07-04 21:28:01 +0300

    New tests / workflow

--- src/application.py
+++ src/application.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask import render_template
+import os
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! v1.1"
+    #print(os.getcwd())
+    return render_template("index.html",greeting="Artur")
+    #return "Hello World! v1.1"
 
 # run the app.
 if __name__ == "__main__":
     application.run()
+
+
---
+++ src/static/open_close.js
@@ -0,0 +1,26 @@
+function w3_open() {
+  var x = document.getElementById("mySidebar");
+  x.style.width = "100%";
+  x.style.fontSize = "40px";
+  x.style.paddingTop = "10%";
+  x.style.display = "block";
+}
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+var mybtn = document.getElementsByClassName("testbtn")[0];
+mybtn.click();
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 5;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width == 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1  + '%';
+    }
+  }
+}
